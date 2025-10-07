b71cc1dec2e668ca4c2f1e03081b1eecc6a420eb
samiha-mahin <samihamhin36@example.com>
2025-10-08 01:56:50 +0600

    seren

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,4 +1,32 @@
 import { Request , Response } from "express";
 import { User } from "@/models/User";
+import { Session } from "@/models/Session";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+
+export const register = async (req: Request, res: Response) =>{
+    try {
+        const {name, email, password} = req.body;
+        if (!name || !email || !password) {
+            return res.status(400).json({message: "All fields are required"});
+        }
+        const existingUser = await User.findOne({email});
+        if (existingUser){
+            return res.status(409).json({message: "User already exists"})
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = new User({name, email, password: hashedPassword});
+        await user.save();
+        res.status(201).json({
+            user:{
+                _id: user._id,
+                name: user.name,
+                email: user.email
+            },
+            message: "User registered successfully"
+        })
+    } catch (error) {
+        res.status(500).json({message: "Server error", error})
+    }
+}

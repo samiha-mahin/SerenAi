ec5d0d657afcbc0e30cbdcedde513f816e7ac8ca
samiha-mahin <samihamhin36@example.com>
2025-10-08 12:23:43 +0600

    serenai

--- server/src/utils/db.ts
+++ server/src/utils/db.ts
@@ -3,8 +3,8 @@ import { logger } from "./logger";
 
 
 export const connectDB = async () => {
+  const MONGODB_URI = process.env.MONGO_URI || "" ;
   try {
-    const MONGODB_URI = process.env.MONGO_URI as string;
     await mongoose.connect(MONGODB_URI);
     logger.info("Connected to MongoDB Atlas");
   } catch (error) {

44ce6a15fea92e84557d1914b3deacbbd812ff30
samiha-mahin <samihamahin36@gmail.com>
2025-10-12 23:15:01 +0600

    serenai

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -12,7 +12,7 @@ export const register = async (req: Request, res: Response) =>{
         }
         const existingUser = await User.findOne({email});
         if (existingUser){
-            return res.status(409).json({message: "User already exists"})
+            return res.status(409).json({message: "User already exists"}) //409-conflict
         }
         const hashedPassword = await bcrypt.hash(password, 10);
 
@@ -34,6 +34,10 @@ export const register = async (req: Request, res: Response) =>{
 export const login = async (req: Request, res: Response) => {
     try {
         
+        const { email, password} = req.body;
+        if (!email || !password) {
+            return res.status(400).json({message: "Email and password are required"})
+        }
     } catch (error) {
         
     }

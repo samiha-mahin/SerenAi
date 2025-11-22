1df20f3ba8b3463bcb9dd2528f9483e2c34b7f23
samiha-mahin <samihamahin36@gmail.com>
2025-11-22 23:33:27 +0600

    serenai

--- server/src/types/inngest.ts
+++ server/src/types/inngest.ts
@@ -9,10 +9,47 @@ export interface TherapyAgentMemory {
     currentTechnique: string | null;
   };
 }
+
 export interface MessageAnalysis {
   emotionalState: string;
   riskLevel: number;
   themes: string[];
   recommendedApproach: string;
   progressIndicators: string[];
+}
+
+export interface InngestResponse<T> {
+  id: string;
+  data: T;
+}
+
+export interface InngestMessageData {
+  response: string;
+  analysis: MessageAnalysis;
+  updatedMemory?: TherapyAgentMemory;
+}
+
+export interface InngestSessionData {
+  sessionId: string;
+  userId: string;
+  startTime: Date;
+}
+
+export interface InngestEventData {
+  message?: string;
+  history?: any[];
+  memory?: TherapyAgentMemory;
+  goals?: string[];
+  systemPrompt?: string;
+  sessionId?: string;
+  userId?: string;
+  startTime?: Date;
+}
+
+export type InngestMessageResponse = InngestResponse<InngestMessageData>;
+export type InngestSessionResponse = InngestResponse<InngestSessionData>;
+
+export interface InngestEvent {
+  name: string;
+  data: InngestEventData;
 }

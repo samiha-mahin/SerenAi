5b9baac0f7a52a9a3d4d8087f32b62a36d8512a7
samiha-mahin <samihamahin36@gmail.com>
2025-12-15 23:46:55 +0600

    serenai

---
+++ server/src/controllers/chatController.ts
@@ -0,0 +1,4 @@
+import { Request, Response } from "express";
+import { ChatSession, IChatSession } from "../models/ChatSession";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { v4 as uuidv4 } from "uuid";

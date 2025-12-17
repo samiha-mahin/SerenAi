00c6c379e84f50ddc123de226337c7424f21f580
samiha-mahin <samihamahin36@gmail.com>
2025-12-17 23:32:05 +0600

    seren ai

--- server/src/controllers/chatController.ts
+++ server/src/controllers/chatController.ts
@@ -2,3 +2,7 @@ import { Request, Response } from "express";
 import { ChatSession, IChatSession } from "../models/ChatSession";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { v4 as uuidv4 } from "uuid";
+import { inngest } from "../inngest/client";
+import { User } from "../models/User";
+import { InngestSessionResponse, InngestEvent } from "../types/inngest";
+import { Types } from "mongoose";
